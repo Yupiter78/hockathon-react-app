49f38f07ffd9816b440920276e7f55e0e76eb126
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-10-09 20:18:15 +0300

    added breadcrumbs__4

--- src/components/breadcrumbs/breadcrumbs.jsx
+++ src/components/breadcrumbs/breadcrumbs.jsx
@@ -3,7 +3,6 @@ import { Link, useLocation } from "react-router-dom";
 
 const Breadcrumbs = () => {
     const location = useLocation();
-    console.log("location.pathname:", location.pathname);
     const breadCrumbsItems = [
         { to: "/", label: "Home" },
         { to: "/favourites", label: "Favourites" },
@@ -11,22 +10,40 @@ const Breadcrumbs = () => {
         { to: "/about", label: "About" },
         { to: "/:memberId", label: "Member" }
     ];
+
+    const breadCrumbsData = (location) => {
+        if (location !== "/") {
+            return location.split("/").map((crumb) => "/" + crumb);
+        }
+        return ["/"];
+    };
+    const locationArray = breadCrumbsData(location.pathname);
+    const resultData = [];
+    const totalLocation = (arrayPathString, dataPath) => {
+        dataPath.forEach((path) => {
+            arrayPathString.forEach((item) => {
+                if (item === path.to) {
+                    resultData.push(path);
+                }
+            });
+        });
+    };
+    totalLocation(locationArray, breadCrumbsItems);
+
     return (
         <div>
             <nav aria-label="breadcrumb">
                 <ol className="breadcrumb">
-                    {breadCrumbsItems
-                        .filter((item) => item.to === location.pathname)
-                        .map(({ to, label }) => (
-                            <Link
-                                key={to}
-                                to={to}
-                                className="breadcrumb-item active"
-                                aria-current="page"
-                            >
-                                {label}
-                            </Link>
-                        ))}
+                    {resultData.map(({ to, label }) => (
+                        <Link
+                            key={to}
+                            to={to}
+                            className="breadcrumb-item active"
+                            aria-current="page"
+                        >
+                            {label}
+                        </Link>
+                    ))}
                 </ol>
             </nav>
         </div>
