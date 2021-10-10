0275a74f0ee642a89cab9dc7cc7e56b6dc806af8
Yriy Yupiter <yu.piter.gm@gmail.com>
2021-10-10 17:07:25 +0300

    intermediate version__9

--- src/components/member-card/member-card.jsx
+++ src/components/member-card/member-card.jsx
@@ -19,21 +19,27 @@ const MemberCard = ({ memberId, onFavorite }) => {
     badge,
     technologies,
   } = member;
-
   return (
-    <div className="card mb-3">
-      <div className="row g-0">
-        <div className="col-md-4">
-          <img src={photo} className="img-fluid rounded-start" alt="..." />
+    <div className="card mb-3 shadow p-3 page-wrapper">
+      <div className="row g-0 p-3 justify-content-end">
+        <div className="col-md-6">
+          <picture className="member-item-img ">
+            <img
+              src={photo}
+              className="img-fluid rounded-start"
+              alt="..."
+              loading="lazy"
+            />
+          </picture>
         </div>
-        <div className="col-md-8">
-          <Badge color={badge.color} textColor={badge.textColor}>
-            {badge.name}
-          </Badge>
+        <div className="col-md-6 ps-4 pb-4">
           <div className="card-body lead text-muted">
-            <h5 className="card-title">
+            <Badge color={badge.color} textColor={badge.textColor}>
+              {badge.name}
+            </Badge>
+            <h3 className="card-title pt-4">
               {firstName} {lastName}
-            </h5>
+            </h3>
             <p className="card-text">{about}</p>
             {role.text}{' '}
             <ul className="card-text">
@@ -41,34 +47,37 @@ const MemberCard = ({ memberId, onFavorite }) => {
                 <li key={role.componets.length - index}>{component}</li>
               ))}
             </ul>
-            <div>
+            <div className="pb-4">
               {Object.keys(socialLinks).map((key, i) => (
                 <a
                   key={Object.keys(socialLinks).length - i}
                   href={socialLinks[key]}
+                  className="pe-2"
                 >
                   <i className={`bi bi-${key}`}></i>
                 </a>
               ))}
             </div>
-            <div className="d-flex">
+            <div className="d-flex flex-wrap">
               {Object.keys(technologies).map((key, i) => (
                 <Progress
                   key={Object.keys(technologies).length - i}
                   title={technologies[key].name}
-                  value={technologies[key].lavel}
+                  value={technologies[key].level}
                   color={technologies[key].color}
                 />
               ))}
             </div>
-            <Button
-              onClick={() => onFavorite(_id)}
-              cls={`btn btn-outline-${storage[_id] ? 'danger' : 'secondary'}`}
-            >
-              {storage[_id] ? 'Remove' : 'Add to Favorites'}
-            </Button>
           </div>
         </div>
+        <Button
+          onClick={() => onFavorite(_id)}
+          cls={`btn btn-outline-${
+            storage[_id] ? 'danger' : 'secondary'
+          } btn-member-card`}
+        >
+          {storage[_id] ? 'Remove' : 'Add to Favorites'}
+        </Button>
       </div>
     </div>
   );
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 import notFoundPict from '../assets/images/404_notfound.png'
-import { Link } from 'react-router-dom'
+import MenuItem from '../components/navbar/MenuItem'
 
 const PageNotFound = () => {
-	return (
-		<>
-			<div className='page-wrapper'>
-				<img
-					style={{
-						display: 'block',
-						width: '60%',
-						marginLeft: 'auto',
-						marginRight: 'auto',
-					}}
-					src={notFoundPict}
-					alt='Page not found'
-				/>
-			</div>
-			<Link to='/'>Back to Home</Link>
-		</>
-	)
+  return (
+    <>
+      <div className='page-wrapper-404'>
+        <img
+          style={{
+            display: 'block',
+            width: '60%',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+          src={notFoundPict}
+          alt='Page not found'
+        />
+      <div className='block-to-home'>
+        <MenuItem to=''>go to home page :)</MenuItem>
+      </div>
+      </div>
+    </>
+  )
 }
 
 export default PageNotFound
